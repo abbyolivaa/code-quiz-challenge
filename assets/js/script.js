3a131688f92687171e5783479f5159ef4542ba37
Abigail Oliva <abigailoliva@192.168.1.12>
2022-03-28 13:11:45 -0600

    timer deduction and next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,31 @@ var startBtn = document.getElementById('startBtn');
 var startPage = document.getElementById('startPage');
 var quizBox = document.getElementById('quizBox');
 var nextBtn = document.getElementById('nextBtn');
-var questionEl = document.getElementById('ques')
-var answerButtonsEl = document.getElementById('answers')
-var answerBtn = document.getElementById('choice')
+var questionEl = document.getElementById('question')
+var answerButtonsEl = document.getElementById('answerButtons')
+var choiceBtn = document.getElementById('choice')
 var timerEl = document.getElementById('timer')
+var highScorePage = document.getElementById ('highscoreBox')
 
+highScorePage.classList.add('hide');
 
 quizBox.classList.add('hide');
 
-startBtn.addEventListener('click', startQuiz);
-nextBtn.addEventListener('click', setNextQuestion);
-
 let shuffledQues, currentQuesIndex
 
-function timer() {
-    var timeLeft = 60;
+startBtn.addEventListener('click', startQuiz);
 
-    var timeInterval = setInterval(function() {
-
-      if (timeLeft > 0) {
-         timerEl.textContent = "Time left: " + timeLeft
-    
-        timeLeft--;
-      } else if (timeLeft <= 0) {
-        timerEl.textContent = "Time's up!"
-      }else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        displayMessage ();
-      }
-    }, 1000);
-  }
+nextBtn.addEventListener('click', () => {
+    currentQuesIndex++
+    setNextQuestion();
+});
 
 function startQuiz() {
     console.log ('started');
     startPage.classList.add ('hide');
-    quizBox.classList.remove('hide');
     shuffledQues = questions.sort(() => Math.random() - .5);
-    currentQuesIndex = 0
+    currentQuesIndex = 0;
+    quizBox.classList.remove('hide');
     timer();
     setNextQuestion();
 }
@@ -47,65 +34,78 @@ function startQuiz() {
 
 
 function setNextQuestion(){
-    resetState()
+    resetState();
     console.log('next');
-    showQuestion(shuffledQues[currentQuesIndex])
+    showQuestion(shuffledQues[currentQuesIndex]);
 }
 
 function showQuestion(question){
     questionEl.innerText = question.question
     question.answers.forEach(answer => {
-        answerBtn.innerText = answer.text
+        var button = document.createElement('button');
+        button.innerText = answer.text
+        button.classList.add('choice')
         if(answer.correct) {
-            answerBtn.dataset.correct = answer.correct
-        }
-        answerBtn.addEventListener ('click', selectAnswer)
-        answerButtonsEl.appendChild(answerBtn)
-    })
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener ('click', selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
 }
 
 function resetState(){
-    nextBtn.classList.add('hide')
     while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
     }
 }
 
 function selectAnswer(e){
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(answerBtn =>{
-        setStatusClass(button, button.dataset.correct)
-    })
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if(correct){
-       alert("Correct!")
+    var highScore = 0
+    if (answer = correct) {
+        highScore++
+        console.log(highScore);
     }
     else{
-        alert("Wrong")
+        timeLeft -=10;
     }
+    
 }
+var timeLeft = 60
+
+function timer() {
+    timeLeft;
+        var timeInterval = setInterval(function() {
+            if (timeLeft > 0) {
+                timerEl.textContent = "Time left: " + timeLeft;
+                timeLeft--;
+            } else if (timeLeft <= 0) {
+                timerEl.textContent = "Time's up!"
+            }else {
+                timerEl.textContent = '';
+                clearInterval(timeInterval);
+                displayMessage ();
+            }
+
+        }, 1000);
+};
 
 function showHighScores (){
-
+    highScorePage.classList.remove('hide')
 }
 
-function inputInitial () {
 
-}
+//function inputInitial () {}
 
-const questions = [
-    {
+const questions= [
+    {  
         question: 'What does HTML stand for?',
         answers: [
             {text: 'Hybrid Text Markup Language', correct: false},
             {text: 'Hyperlink Tech Markup Language', correct: false},
             {text: 'Hybrid Tech Markup Language', correct: false},
-            {text: 'Hypertext Markup Language', correct: true},
+            {text: 'Hypertext Markup Language', correct: true}
 
         ]
     },
@@ -115,7 +115,7 @@ const questions = [
             {text: 'Categorized stylesheet', correct: false},
             {text: 'Cascading style slide', correct: false},
             {text: 'Chicken', correct: false},
-            {text: 'Cascading stylesheet', correct: true},
+            {text: 'Cascading stylesheet', correct: true}
 
         ]
     },
@@ -125,7 +125,7 @@ const questions = [
             {text: 'flex-direction', correct: false},
             {text: 'flex-wrap', correct: false},
             {text: 'flex-grow', correct: false},
-            {text: 'flex-up', correct: true},
+            {text: 'flex-up', correct: true}
 
         ]
     },
@@ -135,7 +135,7 @@ const questions = [
             {text: 'Bootstrap', correct: false},
             {text: 'jQueryUI', correct: false},
             {text: 'jQuery', correct: false},
-            {text: 'HTML5 library', correct: true},
+            {text: 'HTML5 library', correct: true}
 
         ]
     },
@@ -145,10 +145,10 @@ const questions = [
             {text: 'CSS.com', correct: false},
             {text: 'HTML5 library', correct: false},
             {text: 'jQueryIU', correct: false},
-            {text: 'Bootstrap', correct: true},
+            {text: 'Bootstrap', correct: true}
 
         ]
-    },   
+    }   
 ]
 
 
